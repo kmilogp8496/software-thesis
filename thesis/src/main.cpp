79f95aa1394a33d44b6e6adc76437c80f29390b5
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-16 20:03:40 +0100

    added bme reading and network sent

--- thesis/src/main.cpp
+++ thesis/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include "Environment.h"
+#include "IoTPlatformSensor.h"
 #include <ArduinoJson.h>
 #include "api/api.h"
+#include <Adafruit_BME280.h>
+
+// #define REQUEST_INTERVAL 300000
+#define REQUEST_INTERVAL 3000
+
+Adafruit_BME280 bme; // I2C
 
 void setup()
 {
   Serial.begin(230400);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -22,10 +28,32 @@ void setup()
   Serial.println(WiFi.localIP());
 
   Serial.println("Serial connected");
+
+  Serial.println(F("BME280 test"));
+
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  Serial.println("-- Default Test --");
+
+  Serial.println();
 }
 
 void loop()
 {
-  delay(10000);
-  sendGetRequest("/api/things/" + String(SENSOR_ID));
+  JsonDocument doc;
+  doc[SENSOR_TEMPERATURA_TEMPERATURA_HABITACION] = bme.readTemperature();
+  doc[SENSOR_HUMEDAD_HUMEDAD_HABITACION] = bme.readHumidity();
+  doc[SENSOR_PRESION_PRESION_HABITACION] = bme.readPressure() / 100.0F;
+  
+  platformPushData(doc);
+
+  delay(REQUEST_INTERVAL);
 }
