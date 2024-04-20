d2fd20749a2c8e7a13702be46de2cb7f0bafaaa3
kmilogp8496 <kmilogp8496@gmail.com>
2024-04-20 14:38:56 +0200

    changed CACertificate and domain

--- lib/PlatformApi/PlatformEnvironment.h
+++ lib/PlatformApi/PlatformEnvironment.h
@@ -11,7 +11,7 @@
 #ifndef _APP_ENVIRONMENT_H_
 #define _APP_ENVIRONMENT_H_
 
-#define API_URL "easy-iot.vercel.app"
+#define API_URL "kmilo.dev"
 #define API_PORT 443
 // #define API_URL "192.168.1.138"
 // #define API_PORT 3000
