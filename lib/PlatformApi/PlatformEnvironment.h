d8a37499404fe97b9f2f3483c0939023dccbe5e9
kmilogp8496 <kmilogp8496@gmail.com>
2024-04-26 21:39:54 +0200

    changes on environment

--- lib/PlatformApi/PlatformEnvironment.h
+++ lib/PlatformApi/PlatformEnvironment.h
@@ -11,11 +11,14 @@
 #ifndef _APP_ENVIRONMENT_H_
 #define _APP_ENVIRONMENT_H_
 
+#ifdef CA_CERTIFICATE
 #define API_URL "kmilo.dev"
 #define API_PORT 443
-// #define API_URL "192.168.1.138"
-// #define API_PORT 3000
+#else
+#define API_URL "192.168.1.138"
+#define API_PORT 3000
 
+#endif
 #define AUTH_TOKEN_NAME "nuxt-session="
 
 #endif
